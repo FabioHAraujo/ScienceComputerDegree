016685a2b2274ea8b55da63a12d356c944661646
FabioHAraujo <111090485+FabioHAraujo@users.noreply.github.com>
2022-08-24 22:06:19 -0300

    Create exec6.cpp

---
+++ C++/24-08-22/exec6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int mod, num = 0;
+    cin >> num;
+    if (num%2==0)
+    {
+        cout << "Par";
+    }
+    else
+    {
+        cout << "Impar";
+    }
+    
+    return 0;
+}
