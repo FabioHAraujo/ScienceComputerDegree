3825f700a2e9c88bbcba7c7283d244bb90ebed12
FabioHAraujo <111090485+FabioHAraujo@users.noreply.github.com>
2022-08-24 21:56:03 -0300

    Create or.cpp

---
+++ C++/24-08-22/or.cpp
@@ -0,0 +1,12 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+int main() {
+    int var;
+    std::cin >> var;
+    if (var >= 1 or var <= -5)
+    {
+        std::cout << "Hello world!";
+    }
+    return 0;
+}
