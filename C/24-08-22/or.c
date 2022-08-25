7bc114460f791301372f9725cdbfab1fd84abcaf
FabioHAraujo <111090485+FabioHAraujo@users.noreply.github.com>
2022-08-24 21:55:30 -0300

    Create or.c

---
+++ C/24-08-22/or.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int var;
+    scanf ("%d",&var);
+    if (var >= 1 || var <=-5)
+    {
+        printf("Hello world");
+    }
+    return 0;
+}
