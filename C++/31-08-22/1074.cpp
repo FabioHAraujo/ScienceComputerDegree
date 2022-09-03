1b7a4c24811732ae8d12ad645751016782b78404
FabioHAraujo <fabioh2206@gmail.com>
2022-09-03 13:34:56 -0300

    03-09-2022

--- C++/31-08-22/1074.cpp
+++ C++/31-08-22/1074.cpp
@@ -5,14 +5,39 @@ using namespace std;
 
 int main ()
 {
-    int quant, numero, aux1=0, aux2=0, cont=1;
+    int quant, numero, aux=1;
     cin >> quant;
-    for (quant >1; cont <= quant; cont++)
+    while (aux<=quant)
     {
+        aux++;
         cin >> numero;
-        if (numero >= 1)
+        if (numero==0)
         {
-            aux1++
+            cout << "NULL\n";
+        }
+        else if ((numero%2)==0)
+        {
+            cout << "EVEN ";
+            if (numero>0)
+            {
+                cout << "POSITIVE\n";
+            }
+            else if (numero<0)
+            {
+                cout << "NEGATIVE\n";
+            }
+        }
+        else if ((numero%2)==1 || (numero%2)==-1)
+        {
+            cout << "ODD ";
+                if (numero>0)
+                {
+                    cout << "POSITIVE\n";
+                }
+                else if (numero<0)
+                {
+                    cout << "NEGATIVE\n";
+                }
         }
     }
     
--- C++/31-08-22/1099.cpp
+++ C++/31-08-22/1099.cpp
@@ -5,25 +5,53 @@ using namespace std;
 
 int main ()
 {
-    int aux=1, vezes, X, Y, auxX, valorX, auxY, valorX, valorY;
+    int aux=1, vezes=0, X=0, Y=0, maior=0, menor=0, count=0, soma=0;
     cin >> vezes;
-    while (aux!=vezes)
+    while (aux<=vezes)
     {
-        aux++;
         cin >> X >> Y;
-        if (X > Y)
+        aux++;
+        if (X>Y)
         {
-            while (X < Y)
+            maior = X;
+            menor = Y;
+            count = menor;
+            while (menor < (maior-1))
             {
-                X++;
-                auxX = X%2;
-                if (auxX==1)
-                {
-                    valorX = X;
+                menor++;
+                count++;
+                if ((menor%2)==1)
+                {  
+                    soma+=count;
                 }
             }
+            cout << soma << endl;
+            soma = 0;
         }
-    }
+        else if (Y>X)
+        {
+            maior = Y;
+            menor = X;
+            count = menor;
+            while (menor < (maior-1))
+            {
+                menor++;
+                count++;
+                if ((menor%2)==1)
+                {  
+                    soma+=count;
+                }
+            }
+            cout << soma << endl;
+            soma = 0;
+        }
+        else
+        {
+            cout << "0" << endl;
+        }
+        
+        }
+
     
     return 0;
 }
