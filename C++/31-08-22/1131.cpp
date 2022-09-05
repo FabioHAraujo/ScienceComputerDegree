53f6d21f160f6d1819e6e81d81e566d76fe9ea11
FabioHAraujo <fabioh2206@gmail.com>
2022-09-05 10:13:29 -0300

    05-09-2022

--- C++/31-08-22/1131.cpp
+++ C++/31-08-22/1131.cpp
@@ -5,14 +5,41 @@ using namespace std;
 
 int main ()
 {
-    int grenais, opcao=1, gremio, inter;
+    int grenais=0, opcao=1, gremio, inter, colorado=0, tricolor=0, empate=0;
     for (opcao=1; opcao <2; grenais++)
     {
         cin >> inter >> gremio;
-        cout << "Novo grenal (1-sim 2-nao)";
+        cout << "Novo grenal (1-sim 2-nao)" << endl;
         cin >> opcao;
+        if (inter>gremio)
+        {
+            colorado++;
+        }
+        else if (gremio>inter)
+        {
+            tricolor++;
+        }
+        else
+        {
+            empate++;
+        }
+    }
+    cout << grenais << " grenais" << endl;
+    cout << "Inter: " << colorado << endl;
+    cout << "Gremio: " << tricolor << endl;
+    cout << "Empates: " << empate << endl;
+    if (inter>gremio)
+    {
+        cout << "Inter venceu mais" << endl;
+    }
+    else if (gremio>inter)
+    {
+        cout << "Gremio venceu mais" << endl;
+    }
+    else
+    {
+        cout << "Nao houve vencedor" << endl;
     }
 
-    
     return 0;
 }
